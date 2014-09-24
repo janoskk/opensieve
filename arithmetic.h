e0fd720ed5bc1be8d1b83660d58b860a77141f1e
Janos Kasza <bipketto@gmail.com>
2014-09-24 23:20:29 +0200

    arithmetic.h added

---
+++ arithmetic.h
@@ -0,0 +1,104 @@
+/*
+ *  Copyright 2014 by Janos Kasza
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef ARITHMETIC_H_
+#define ARITHMETIC_H_
+
+#define MASK_R(acc, c, num, num_var, count)   \
+{                                             \
+        num_var >>= count;                    \
+        c = num_var;                          \
+        c <<= num;                            \
+        num_var |= c;                         \
+        acc |= num_var;                       \
+}
+
+#define MASK_L(acc, c, num, num_var, count)   \
+{                                             \
+        num_var <<= count;                    \
+        c = num_var;                          \
+        c >>= num;                            \
+        num_var |= c;                         \
+        acc |= num_var;                       \
+}
+
+
+/************************************************************************************/
+/*
+ * Replacement for num >> bits computation what is limited to shift maximum 31 bits
+ */
+inline uint64_t jkk_asm_shl(uint64_t num, int bits)
+{
+    __asm__("shlq %%cl, %%rax;"
+        : "=a" (num)
+                    : "a" (num), "c" (bits)
+                   );
+    return num;
+}
+
+
+/************************************************************************************/
+/*
+ * Replacement for num << bits computation what is limited to shift maximum 31 bits
+ */
+inline uint64_t jkk_asm_shr(uint64_t num, int bits)
+{
+    __asm__("shrq %%cl, %%rax;"
+        : "=a" (num)
+                    : "a" (num), "c" (bits)
+                   );
+    return num;
+}
+
+/************************************************************************************/
+/*
+ * Rotate to left instruction
+ */
+inline uint64_t jkk_asm_rol(uint64_t num, int bits)
+{
+    __asm__("rolq %%cl, %%rax;"
+        : "=a" (num)
+                    : "a" (num), "c" (bits)
+                   );
+    return num;
+}
+
+
+/************************************************************************************/
+/*
+ * Rotate to right instruction
+ */
+inline uint64_t jkk_asm_ror(uint64_t num, int bits)
+{
+    __asm__("rorq %%cl, %%rax;"
+        : "=a" (num)
+                    : "a" (num), "c" (bits)
+                   );
+    return num;
+}
+
+
+/************************************************************************************/
+inline uint64_t jkk_asm_bsf(uint64_t num)
+{
+    uint64_t ret = 0;
+    asm("bsf %1, %0;"
+    :"=r" (ret)     /* output */
+                :"r" (num)      /* input */);
+    return ret;
+}
+
+#endif
--- opensieve.cc
+++ opensieve.cc
@@ -22,83 +22,26 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 
+#include "arithmetic.h"
 #include "opensieve.h"
 
 #ifndef SEGMENT_BITS
-  #define SEGMENT_BITS 10
+#define SEGMENT_BITS 10
 #endif
 #define SEGMENT_SIZE (1<<SEGMENT_BITS)
 //#define SEGMENT_SIZE 1000000000
 
-
 char wheel30[] =
-    { 1, 6, 5, 4, 3, 2, 1,
-      4, 3, 2, 1, 2, 1,
-      4, 3, 2, 1, 2, 1,
-      4, 3, 2, 1, 6, 5, 4, 3, 2, 1, 2 };
+{ 1, 6, 5, 4, 3, 2, 1, 4, 3, 2, 1, 2, 1, 4, 3, 2, 1, 2, 1, 4, 3, 2, 1, 6, 5, 4,
+        3, 2, 1, 2 };
 
-char rem30[] = { 1,  7, 11, 13, 17, 19, 23, 29 };
-char wow30[] = { 6,  4,  2,  4,  2,  4,  6,  2 };
-char inv30[] = { -1, 0, -1, -1, -1, -1, -1, 1, -1, -1,
-                 -1, 2, -1, 3, -1, -1, -1, 4, -1, 5,
-                 -1, -1, -1, 6, -1, -1, -1, -1, -1, 7 };
-
-/************************************************************************************/
-/*
- * Replacement for num >> bits computation what is limited to shift maximum 31 bits
- */
-inline uint64_t jkk_asm_shl(uint64_t num, int bits)
-{
-    __asm__("shlq %%cl, %%rax;"
-        : "=a" (num)
-                    : "a" (num), "c" (bits)
-                   );
-    return num;
-}
-
-
-/************************************************************************************/
-/*
- * Replacement for num << bits computation what is limited to shift maximum 31 bits
- */
-inline uint64_t jkk_asm_shr(uint64_t num, int bits)
-{
-    __asm__("shrq %%cl, %%rax;"
-        : "=a" (num)
-                    : "a" (num), "c" (bits)
-                   );
-    return num;
-}
-
-
-/************************************************************************************/
-inline uint64_t jkk_asm_bsf(uint64_t num)
-{
-    uint64_t ret = 0;
-    asm("bsf %1, %0;"
-    :"=r" (ret)     /* output */
-                :"r" (num)      /* input */);
-    return ret;
-}
-
-#define MASK_R(num, num_var, count)   \
-{                                     \
-        num_var >>= count;                \
-        c = num_var;                  \
-        c <<= num;                    \
-        num_var |= c;                 \
-        acc |= num_var;               \
-}
-
-#define MASK_L(num, num_var, count)   \
-{                                     \
-        num_var <<= count;                \
-        c = num_var;                  \
-        c >>= num;                    \
-        num_var |= c;                 \
-        acc |= num_var;               \
-}
-
+char rem30[] =
+{ 1, 7, 11, 13, 17, 19, 23, 29 };
+char wow30[] =
+{ 6, 4, 2, 4, 2, 4, 6, 2 };
+char inv30[] =
+{ -1, 0, -1, -1, -1, -1, -1, 1, -1, -1, -1, 2, -1, 3, -1, -1, -1, 4, -1, 5, -1,
+        -1, -1, 6, -1, -1, -1, -1, -1, 7 };
 
 /************************************************************************************/
 void bitsieve(uint64_t table[], unsigned length)
@@ -122,68 +65,65 @@ void bitsieve(uint64_t table[], unsigned length)
 
     for (unsigned i = 1; i < length; i++)
     {
-        register uint64_t c = 0;
         register uint64_t acc = 0; // !!
+        register uint64_t c = 0;
 
-        MASK_R(3, a_3, 1);
-        MASK_L(5, a_5, 1);
-        MASK_R(7, a_7, 1);
-        MASK_L(11, a_11, 2);
-        MASK_L(13, a_13, 1);
-        MASK_L(17, a_17, 4);
-        MASK_R(19, a_19, 7);
+        MASK_R(acc, c, 3, a_3, 1);
+        MASK_L(acc, c, 5, a_5, 1);
+        MASK_R(acc, c, 7, a_7, 1);
+        MASK_L(acc, c, 11, a_11, 2);
+        MASK_L(acc, c, 13, a_13, 1);
+        MASK_L(acc, c, 17, a_17, 4);
+        MASK_R(acc, c, 19, a_19, 7);
 
         table[i] = acc;
     }
 }
 
+/************************************************************************************/
+#define IS_PRIME_AT(is_prime, x) {                              \
+	uint32_t seg = x >> 6;                                      \
+	uint64_t off = jkk_asm_shl(1ULL,  /* 0x3f - */ (x & 0x3f)); \
+	is_prime = ((*table)[seg] & off) == 0;                      \
+}
 
 /************************************************************************************/
-#define IS_PRIME_AT(is_prime, x) {					\
-	uint32_t seg = x >> 6;						\
-	uint64_t off = jkk_asm_shl(1ULL,  /* 0x3f - */ (x & 0x3f));	\
-	is_prime = ((*table)[seg] & off) == 0;				\
-    }
-
-
-/************************************************************************************/
-#define SIEVE_AT(x) {					\
-	seg = pos >> 6;					\
-	off = jkk_asm_shl(1ULL,  (pos & 0x3f));		\
-	(*table)[seg] |= off;				\
-	pos += pos30[x];				\
-	j++;						\
-    }
-
+#define SIEVE_AT(x) {                                           \
+	seg = pos >> 6;                                             \
+	off = jkk_asm_shl(1ULL,  (pos & 0x3f));                     \
+	(*table)[seg] |= off;                                       \
+	pos += pos30[x];                                            \
+	j++;                                                        \
+}
 
 /************************************************************************************/
 void open_sieve(uint64_t limit, uint64_t **table, uint64_t& table_size)
 {
     table_size = (((limit + 1) >> 1) + 63) >> 6;
-    *table = (uint64_t*)valloc(table_size * sizeof(uint64_t));
-    const uint64_t sqrt_limit = sqrt((double)limit) + 1;
+    *table = (uint64_t*) valloc(table_size * sizeof(uint64_t));
+    const uint64_t sqrt_limit = sqrt((double) limit) + 1;
 
     /*
-    printf("limit      = %" PRIu64 "u\n", limit);
-    printf("sqrt_limit = %" PRIu64 "u\n", sqrt_limit);
-    printf("table_size = %" PRIu64 "u\n", table_size);
-    */
+     printf("limit      = %" PRIu64 "u\n", limit);
+     printf("sqrt_limit = %" PRIu64 "u\n", sqrt_limit);
+     printf("table_size = %" PRIu64 "u\n", table_size);
+     */
 
     bitsieve(*table, table_size);
-    (*table)[0] &= 0xffffffffffffffff ^ (2|4|8|32|64|256|512); // correcting 3, 5, 7, 11, 13, 17, 19
+    (*table)[0] &= 0xffffffffffffffff ^ (2 | 4 | 8 | 32 | 64 | 256 | 512); // correcting 3, 5, 7, 11, 13, 17, 19
 
-    for (uint64_t i = 17; i <= sqrt_limit; i+=2)
+    for (uint64_t i = 23; i <= sqrt_limit; i += 2)
     {
         int is_prime = 0;
-        IS_PRIME_AT(is_prime, (i-1)/2);
+        IS_PRIME_AT(is_prime, (i - 1) / 2);
         if (is_prime)
         {
 #define VERSION 1
 #if VERSION == 1
-            for (uint64_t j = (i * i - 1) / 2; j <= limit/2; j += i)
+            for (uint64_t j = (i * i - 1) / 2; j <= limit / 2; j += i)
             {
                 uint32_t seg = j >> 6;
-                uint64_t off = jkk_asm_shl(1ULL,  (j & 0x3f));
+                uint64_t off = jkk_asm_shl(1ULL, (j & 0x3f));
                 (*table)[seg] |= off;
             }
 #elif VERSION == 2
@@ -191,7 +131,7 @@ void open_sieve(uint64_t limit, uint64_t **table, uint64_t& table_size)
             for (uint64_t j = i; pos <= limit/2; j += wheel30[j % 30])
             {
                 uint32_t seg = pos >> 6;
-                uint64_t off = jkk_asm_shl(1ULL,  (pos & 0x3f));
+                uint64_t off = jkk_asm_shl(1ULL, (pos & 0x3f));
                 (*table)[seg] |= off;
                 pos = (i * j - 1) / 2;
             }
@@ -211,7 +151,7 @@ void open_sieve(uint64_t limit, uint64_t **table, uint64_t& table_size)
             while ( pos <= limit/2 )
             {
                 uint32_t seg = pos >> 6;
-                uint64_t off = jkk_asm_shl(1ULL,  (pos & 0x3f));
+                uint64_t off = jkk_asm_shl(1ULL, (pos & 0x3f));
                 (*table)[seg] |= off;
                 pos += wow30prime[ j & 7 ];
                 j++;
@@ -270,15 +210,12 @@ void open_sieve(uint64_t limit, uint64_t **table, uint64_t& table_size)
     }
 }
 
-
 /************************************************************************************/
-uint64_t process_primes(SIEVE_PROCESS_FUNC *process_for_primes,
-                    uint64_t *table,
-                    uint64_t table_size,
-                    unsigned current_segment)
+uint64_t process_primes(SIEVE_PROCESS_FUNC *process_for_primes, uint64_t *table,
+        uint64_t table_size, unsigned current_segment)
 {
-	uint64_t prime = 0;
-    for(uint64_t i = 0; i < table_size; i++)
+    uint64_t prime = 0;
+    for (uint64_t i = 0; i < table_size; i++)
     {
         uint64_t num = table[i];
         if (num != 0)
@@ -288,7 +225,8 @@ uint64_t process_primes(SIEVE_PROCESS_FUNC *process_for_primes,
             {
                 if ((num & pos) != pos)
                 {
-                    prime = (current_segment * SEGMENT_SIZE) + (((i << 6) + off) << 1) + 1;
+                    prime = (current_segment * SEGMENT_SIZE)
+                            + (((i << 6) + off) << 1) + 1;
                     prime = (prime == 1) ? 2 : prime;
                     if (process_for_primes != 0)
                     {
@@ -302,9 +240,9 @@ uint64_t process_primes(SIEVE_PROCESS_FUNC *process_for_primes,
     return prime;
 }
 
-
 /************************************************************************************/
-static uint64_t get_diff(uint64_t *table, uint64_t table_size, uint64_t &seg, uint64_t &pos)
+static uint64_t get_diff(uint64_t *table, uint64_t table_size, uint64_t &seg,
+        uint64_t &pos)
 {
     pos <<= 1;
     seg = (pos == 0) ? seg + 1 : seg;
@@ -313,7 +251,7 @@ static uint64_t get_diff(uint64_t *table, uint64_t table_size, uint64_t &seg, ui
     int diff = 2;
     for (; seg < table_size; seg++)
     {
-        for	(; pos != 0; pos <<= 1)
+        for (; pos != 0; pos <<= 1)
         {
             if ((num & pos) != pos)
             {
@@ -325,27 +263,25 @@ static uint64_t get_diff(uint64_t *table, uint64_t table_size, uint64_t &seg, ui
     return 0;
 }
 
-
 /************************************************************************************/
-void segmented_sieve(int64_t first_segment,
-                     int no_of_segments,
-                     SIEVE_PROCESS_FUNC *process_for_primes)
+void segmented_sieve(int64_t first_segment, int no_of_segments,
+        SIEVE_PROCESS_FUNC *process_for_primes)
 {
     uint64_t first = first_segment * SEGMENT_SIZE + 1;
     uint64_t last = (first_segment + no_of_segments) * SEGMENT_SIZE;
-    uint64_t sqrt_of_last_elem = (int64_t) sqrt((double)last) + 2;
+    uint64_t sqrt_of_last_elem = (int64_t) sqrt((double) last) + 2;
 
     uint64_t *small_primes;
     uint64_t small_primes_size;
     open_sieve(sqrt_of_last_elem, &small_primes, small_primes_size);
 
-    uint64_t segment[SEGMENT_SIZE>>7];
+    uint64_t segment[SEGMENT_SIZE >> 7];
 
     uint64_t n = (first == 1) ? 3 : first;
     uint64_t segment_no = first_segment;
     while (n < last)
     {
-        memset(segment, 0x0, (SEGMENT_SIZE>>7) * sizeof(uint64_t));
+        memset(segment, 0x0, (SEGMENT_SIZE >> 7) * sizeof(uint64_t));
         uint64_t segment_first = segment_no * SEGMENT_SIZE + 1;
         uint64_t segment_last = segment_no * SEGMENT_SIZE + SEGMENT_SIZE;
         uint64_t seg = 0;
@@ -359,7 +295,7 @@ void segmented_sieve(int64_t first_segment,
             uint64_t next = prime * prime;
             if (next > segment_last)
             {
-            	continue;
+                continue;
             }
             if (segment_first > next) // almost always, except the smaller primes and in the beginning of sieve
             {
@@ -371,18 +307,19 @@ void segmented_sieve(int64_t first_segment,
             }
             //printf("prime: %llu next: %llu\n", prime, next);
 
-            next = ((next-1) >> 1) % (SEGMENT_SIZE>>1);
+            next = ((next - 1) >> 1) % (SEGMENT_SIZE >> 1);
             while (next < SEGMENT_SIZE >> 1)
             {
                 uint32_t seg = next >> 6;
-                uint64_t off = jkk_asm_shl(1ULL,  next & 0x3f);
+                uint64_t off = jkk_asm_shl(1ULL, next & 0x3f);
                 segment[seg] |= off;
 
                 next += prime;
             }
         }
 
-        n = process_primes(process_for_primes, segment, SEGMENT_SIZE >> 7, segment_no);
+        n = process_primes(process_for_primes, segment, SEGMENT_SIZE >> 7,
+                segment_no);
         //printf("utolso prim ebben a segment-ben: %llu\n", n);
 
         segment_no++;
